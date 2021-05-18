0c688758bee9d54451f9507ed384f9d2e5cb25a3
icortez <icortez@ufg.edu.sv>
2021-05-18 13:55:53 -0600

    building basic setup for restaurant info component with mock info

--- src/features/restaurants/components/restaurant-info-component.js
+++ src/features/restaurants/components/restaurant-info-component.js
@@ -1,4 +1,17 @@
 import React from 'react';
 import { Text } from 'react-native';
 
-export const RestaurantInfo = () => <Text>Restaurant info</Text>;
+export const RestaurantInfo = ({ restaurant = {} }) => {
+  const {
+    name = 'Some Restaurant',
+    icon,
+    photos = [
+      'https://ep01.epimg.net/economia/imagenes/2019/11/12/actualidad/1573563004_166027_1573571338_noticia_normal.jpg',
+    ],
+    address = '100 some random street',
+    isOpenNow = true,
+    rating = 4,
+    isClosedTemporarily,
+  } = restaurant;
+  return <Text>{name}</Text>;
+};
