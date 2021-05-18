176f60d9274e1e7c3bff7f8db312117a69adec7f
icortez <icortez@ufg.edu.sv>
2021-05-18 14:13:12 -0600

    building a nice card from react native paper with mock info

--- src/features/restaurants/components/restaurant-info-component.js
+++ src/features/restaurants/components/restaurant-info-component.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, StyleSheet } from 'react-native';
+import { Card } from 'react-native-paper';
 
 export const RestaurantInfo = ({ restaurant = {} }) => {
   const {
@@ -13,5 +14,24 @@ export const RestaurantInfo = ({ restaurant = {} }) => {
     rating = 4,
     isClosedTemporarily,
   } = restaurant;
-  return <Text>{name}</Text>;
+
+  return (
+    <Card elevation={5} style={styles.card}>
+      <Card.Cover key={name} source={{ uri: photos[0] }} style={styles.cover} />
+      <Text style={styles.title}>{name}</Text>
+    </Card>
+  );
 };
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: 'white',
+  },
+  cover: {
+    padding: 20,
+    backgroundColor: 'white',
+  },
+  title: {
+    padding: 16,
+  },
+});
