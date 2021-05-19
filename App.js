6a1aa83f0d64f00a4f9a010b9b8133dfb9afc5c9
Italo Stuardo Cortez Silva <ingeniero.stuardo@gmail.com>
2021-05-19 05:25:47 -0600

    adding infrastructure/theme folders to include global styles and adding theme provider to app.js to use those global styles and converting all styles to styled components

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import { ThemeProvider } from 'styled-components/native';
 
+import { theme } from './src/infrastructure/theme';
 import { RestaurantScreen } from './src/features/restaurants/screens/restaurants.screen';
 
 export default function App() {
   return (
     <>
-      <RestaurantScreen />
-      <ExpoStatusBar style='auto' />
+      <ThemeProvider theme={theme}>
+        <RestaurantScreen />
+        <ExpoStatusBar style='auto' />
+      </ThemeProvider>
     </>
   );
 }
