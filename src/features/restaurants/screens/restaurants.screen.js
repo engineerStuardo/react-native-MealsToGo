454eaab73dea5d516d250dcd3ed6c332e4153469
icortez <icortez@ufg.edu.sv>
2021-05-20 15:28:26 -0600

    setting up a FlatList in restaurant.sscreen

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { FlatList } from 'react-native';
 import { Searchbar } from 'react-native-paper';
 
 import { RestaurantInfoCard } from '../components/restaurant-info-card';
+import { Spacer } from '../../../components/spacer/spacer-component';
 import {
   SafeArea,
   SearchContainer,
@@ -13,8 +15,30 @@ export const RestaurantScreen = () => (
     <SearchContainer>
       <Searchbar />
     </SearchContainer>
-    <RestaurantListContainer>
-      <RestaurantInfoCard />
-    </RestaurantListContainer>
+    <FlatList
+      data={[
+        { name: 1 },
+        { name: 2 },
+        { name: 3 },
+        { name: 4 },
+        { name: 5 },
+        { name: 6 },
+        { name: 7 },
+        { name: 8 },
+        { name: 9 },
+        { name: 10 },
+        { name: 11 },
+        { name: 12 },
+        { name: 13 },
+        { name: 14 },
+      ]}
+      renderItem={() => (
+        <Spacer position='bottom' size='large'>
+          <RestaurantInfoCard />
+        </Spacer>
+      )}
+      keyExtractor={item => item.name}
+      contentContainerStyle={{ padding: 16 }}
+    />
   </SafeArea>
 );
