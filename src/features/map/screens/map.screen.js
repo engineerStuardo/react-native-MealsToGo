0f80b9651f0954a4e3f8808bb7f116e35190ac03
icortez <icortez@ufg.edu.sv>
2021-05-27 15:32:25 -0600

    setting the map region dynamically so when the user change locacion in restaurant or map we change the view in the map

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -1,17 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import MapView from 'react-native-maps';
 import styled from 'styled-components/native';
 
 import { Search } from '../components/search-component';
+import { useLocationContext } from '../../../services/location/customHook';
+import { useRestaurantsContext } from '../../../services/restaurants/customHook';
 
 const Map = styled(MapView)`
   height: 100%;
   width: 100%;
 `;
 
-export const MapScreen = () => (
-  <>
-    <Search />
-    <Map />
-  </>
-);
+export const MapScreen = () => {
+  const { location } = useLocationContext();
+  const { restaurants = [] } = useRestaurantsContext();
+  const [latDelta, setLatDelta] = useState(0);
+  const { viewport, lat, lng } = location;
+
+  useEffect(() => {
+    const northeastLat = viewport.northeast.lat;
+    const southwestLat = viewport.southwest.lat;
+
+    setLatDelta(northeastLat - southwestLat);
+  }, [location, viewport]);
+
+  return (
+    <>
+      <Search />
+      <Map
+        region={{
+          latitude: lat,
+          longitude: lng,
+          latitudeDelta: latDelta,
+          longitudeDelta: 0.02,
+        }}
+      >
+        {restaurants.map(restaurant => {
+          return null;
+        })}
+      </Map>
+    </>
+  );
+};
