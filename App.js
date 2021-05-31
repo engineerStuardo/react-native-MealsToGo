e78c13543c4da417e1a746f1c1e02f5fb6e63c17
Italo Stuardo Cortez Silva <ingeniero.stuardo@gmail.com>
2021-05-31 04:24:06 -0600

    installing expo firebase and start the basic config in app.js

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import {
   Oswald_400Regular,
 } from '@expo-google-fonts/oswald';
 import { useFonts as useLato, Lato_400Regular } from '@expo-google-fonts/lato';
+import firebase from 'firebase/app';
 
 import { theme } from './src/infrastructure/theme';
 import { SafeArea } from './src/components/utility/safe-area-component';
@@ -13,6 +14,17 @@ import { RestaurantProvider } from './src/services/restaurants/restaurants-conte
 import { LocationProvider } from './src/services/location/location-context';
 import { FavouritesProvider } from './src/services/favourites/favourites-context';
 import { Navigation } from './src/infrastructure/navigation';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyA0oUUEQKPy_VBq1-yOig_ij4-rj-fa0ac',
+  authDomain: 'mealstogo-5fec5.firebaseapp.com',
+  projectId: 'mealstogo-5fec5',
+  storageBucket: 'mealstogo-5fec5.appspot.com',
+  messagingSenderId: '295306349466',
+  appId: '1:295306349466:web:74136a3e554ddcc4cfc823',
+};
+
+firebase.initializeApp(firebaseConfig);
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
