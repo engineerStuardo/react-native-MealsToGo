7ce334e93627abbf989a8b7880cf73ef2fceb60b
icortez <icortez@ufg.edu.sv>
2021-06-04 09:43:49 -0600

    installing expo camera and building a component to change the avatar icon to a real photo using the mobile camera

--- src/features/settings/screens/settings.screen.js
+++ src/features/settings/screens/settings.screen.js
@@ -1,7 +1,9 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState } from 'react';
+import { View, TouchableOpacity } from 'react-native';
 import { List, Avatar } from 'react-native-paper';
 import styled from 'styled-components/native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useFocusEffect } from '@react-navigation/native';
 
 import { useAuthenticationContext } from '../../../services/authentication/customHook';
 import { Text } from '../../../components/typography/text-component';
@@ -17,19 +19,42 @@ const AvatarContainer = styled.View`
 
 export const SettingsScreen = ({ navigation }) => {
   const { onLogout, user } = useAuthenticationContext();
+  const [photo, setPhoto] = useState(null);
+
+  const getProfilePicture = async currentUser => {
+    const photoUri = AsyncStorage.getItem(`${currentUser.uid}-photo`);
+    setPhoto(photoUri);
+  };
+
+  useFocusEffect(() => {
+    getProfilePicture(user);
+  }, [user]);
+
   return (
     <View>
       <List.Section>
-        <AvatarContainer>
-          <Avatar.Icon
-            size={180}
-            icon='human'
-            style={{ backgroundColor: '#2182BD' }}
-          />
-          <Spacer position='top' size='large'>
-            <Text variant='caption'>{user.email}</Text>
-          </Spacer>
-        </AvatarContainer>
+        <TouchableOpacity onPress={navigation.navigate('Camera')}>
+          <AvatarContainer>
+            {!photo ? (
+              <Avatar.Icon
+                size={180}
+                icon='human'
+                style={{ backgroundColor: '#2182BD' }}
+              />
+            ) : (
+              <Avatar.Image
+                size={180}
+                source={{ uri: photo }}
+                style={{ backgroundColor: '#2182BD' }}
+              />
+            )}
+
+            <Spacer position='top' size='large'>
+              <Text variant='caption'>{user.email}</Text>
+              <Text variant='caption'>{user.email}</Text>
+            </Spacer>
+          </AvatarContainer>
+        </TouchableOpacity>
         <SettingsItem
           title='Favourites'
           description='View your favourites'
