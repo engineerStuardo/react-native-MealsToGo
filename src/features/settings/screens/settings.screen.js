2bfb2c00007f07fc99546161fcde4d5dad8f0fc2
Italo Stuardo Cortez Silva <ingeniero.stuardo@gmail.com>
2021-06-05 05:09:15 -0600

    refactoring style in settings screen

--- src/features/settings/screens/settings.screen.js
+++ src/features/settings/screens/settings.screen.js
@@ -12,10 +12,26 @@ import { colors } from '../../../infrastructure/theme/colors';
 
 const SettingsItem = styled(List.Item)`
   padding: ${props => props.theme.space[2]};
+  margin-bottom: ${props => props.theme.space[2]};
+  background-color: rgba(33, 130, 189, 0.15);
 `;
 
 const AvatarContainer = styled.View`
   align-items: center;
+`;
+
+const SettingsBackground = styled.ImageBackground.attrs({
+  source: require('../../../../assets/home_bg.jpg'),
+})`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+`;
+
+const TransparentSafeArea = styled(View)`
+  flex: 1;
+  z-index: 999;
+  background-color: rgba(255, 255, 255, 0.8);
 `;
 
 export const SettingsScreen = ({ navigation }) => {
@@ -41,53 +57,68 @@ export const SettingsScreen = ({ navigation }) => {
   };
 
   return (
-    <View>
-      <List.Section>
-        <Spacer position='top' size='large' />
-        <Spacer position='top' size='large' />
-        <TouchableOpacity onPress={() => navigation.navigate('Camera')}>
-          <AvatarContainer>
-            {!photo && (
-              <Avatar.Icon
-                size={180}
-                icon='human'
-                style={{ backgroundColor: colors.brand.primary }}
+    <>
+      <TransparentSafeArea>
+        <List.Section>
+          <Spacer position='top' size='large' />
+          <Spacer position='top' size='large' />
+          <TouchableOpacity onPress={() => navigation.navigate('Camera')}>
+            <AvatarContainer>
+              {!photo && (
+                <Avatar.Icon
+                  size={180}
+                  icon='human'
+                  style={{ backgroundColor: colors.brand.primary }}
+                />
+              )}
+              {photo && (
+                <Avatar.Image
+                  size={180}
+                  source={{ uri: photo }}
+                  style={{ backgroundColor: colors.brand.primary }}
+                />
+              )}
+              <Text variant='caption'>(Tap to change photo)</Text>
+              <Spacer position='top' size='large'>
+                <Text variant='body'>Welcome, {user.email}</Text>
+              </Spacer>
+              <Spacer position='bottom' size='large' />
+            </AvatarContainer>
+          </TouchableOpacity>
+          <SettingsItem
+            title='Favourites'
+            description='View your favourites'
+            left={props => (
+              <List.Icon {...props} color={colors.ui.error} icon='heart' />
+            )}
+            onPress={() => navigation.navigate('Favourites')}
+          />
+          <SettingsItem
+            title='Delete photo'
+            description='Delete your profile photo'
+            left={props => (
+              <List.Icon
+                {...props}
+                color={colors.brand.secondary}
+                icon='delete-forever'
               />
             )}
-            {photo && (
-              <Avatar.Image
-                size={180}
-                source={{ uri: photo }}
-                style={{ backgroundColor: colors.brand.primary }}
+            onPress={() => deleteProfilePhoto()}
+          />
+          <SettingsItem
+            title='Logout'
+            left={props => (
+              <List.Icon
+                {...props}
+                color={colors.brand.secondary}
+                icon='logout'
               />
             )}
-            <Text variant='caption'>(Tap to change photo)</Text>
-            <Spacer position='top' size='large'>
-              <Text variant='body'>Welcome, {user.email}</Text>
-            </Spacer>
-            <Spacer position='bottom' size='large' />
-          </AvatarContainer>
-        </TouchableOpacity>
-        <SettingsItem
-          title='Favourites'
-          description='View your favourites'
-          left={props => <List.Icon {...props} color='#5282BD' icon='heart' />}
-          onPress={() => navigation.navigate('Favourites')}
-        />
-        <SettingsItem
-          title='Delete photo'
-          description='Delete your profile photo'
-          left={props => (
-            <List.Icon {...props} color='#5282BD' icon='delete-forever' />
-          )}
-          onPress={() => deleteProfilePhoto()}
-        />
-        <SettingsItem
-          title='Logout'
-          left={props => <List.Icon {...props} color='#5282BD' icon='logout' />}
-          onPress={onLogout}
-        />
-      </List.Section>
-    </View>
+            onPress={onLogout}
+          />
+        </List.Section>
+      </TransparentSafeArea>
+      <SettingsBackground />
+    </>
   );
 };
